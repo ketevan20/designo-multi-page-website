b65809389ba67f9d29bed3911b58d20584e0b312
ketevan20 <ketevansamukashvili@gmail.com>
2026-03-15 23:44:18 +0400

    feat: add header and footer

---
+++ components/atoms/Header/Header.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+import NavBar from '../NavBar/NavBar'
+
+const Header = () => {
+  return (
+    <div>
+        <NavBar light={true} />
+    </div>
+  )
+}
+
+export default Header
---
+++ components/atoms/NavBar/NavBar.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+
+type NavBarProps = {
+    light: boolean;
+}
+
+const NavBar = ({ light }: NavBarProps) => {
+    return (
+        <div className='w-full flex items-center justify-between'>
+            { light ? <img src='/shared/desktop/logo-dark.png' className='w-49 h-6' /> : <img src='/shared/desktop/logo-light.png' className='w-49 h-6' /> }
+            <div className='flex gap-10.5 uppercase font-normal text-[14px] leading-3.5 tracking-[2px]' style={{color: light ? 'rgba(51, 49, 54, 1)' : 'rgba(255, 255, 255, 1)'}}>
+                <Link href={''} className='hover:underline'>Our Company</Link>
+                <Link href={''} className='hover:underline'>Locations</Link>
+                <Link href={''} className='hover:underline'>Contact</Link>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
