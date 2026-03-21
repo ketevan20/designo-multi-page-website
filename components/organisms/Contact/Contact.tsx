de2274536e41a2c631cac64ad39c2940beb62a3a
ketevan20 <ketevansamukashvili@gmail.com>
2026-03-21 23:55:35 +0400

    feat: complete locations page

--- components/organisms/Contact/Contact.tsx
+++ components/organisms/Contact/Contact.tsx
@@ -1,10 +1,12 @@
 import ContaxtForm from '@/components/molecules/ContactForm/ContaxtForm'
+import LocationsContainer from '@/components/molecules/LocationsContainer/LocationsContainer'
 import React from 'react'
 
 const Contact = () => {
   return (
     <div className='w-[90%] m-auto max-w-480 flex flex-col gap-40 mb-22'>
         <ContaxtForm />
+        <LocationsContainer />
     </div>
   )
 }
